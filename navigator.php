fc9f60283fba0268824635183f23786a3b8d8cb4
SinnoLn <sintmtn69@gmail.com>
2023-12-08 18:25:35 +0900

    qusetpage추가

--- navigator.php
+++ navigator.php
@@ -44,7 +44,7 @@
             <label class="navicon" for="nav-toggle"><span class="navicon-bar"></span></label>
             <ul class="nav-items">
                 <li><a href="../main/home.php">HOME</a></li>
-                <li><a href="#">QUEST</a></li>
+                <li><a href="../quest/questpage.php">QUEST</a></li>
                 <li><a href="#">MYPAGE</a></li>
             </ul>
         </nav>
